96eb5313e153cfa24ab491960f668b00a00871cd
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2021-09-13 11:14:55 -0400

    Added Visitor Interface for dispersing data to TipPool Class

---
+++ TipPooler_POC/Methods/IVisitable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TipPooler_POC.Methods
+{
+    internal interface IVisitable
+    {
+        public void Accept(IVisitor visitor);
+    }
+
+    internal interface IVisitor
+    {
+        public List<T> SupplyData();
+    }
+}
